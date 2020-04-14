9d0453d626ed1258d816daa1ee4acda7c75f16c3
ksk3164 <ksk3164@ex2i.com>
2020-04-14 16:05:07 +0900

    2020-04-14 realm적용

--- app/src/main/java/com/ex2i/fisherman/DetailActivity.kt
+++ app/src/main/java/com/ex2i/fisherman/DetailActivity.kt
@@ -1,23 +1,42 @@
 package com.ex2i.fisherman
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.realm.Realm
+import io.realm.RealmResults
+import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private val realm = Realm.getDefaultInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
         val name = intent.getStringExtra("name")
-        val image = intent.getIntExtra("image",0)
+        val image = intent.getIntExtra("image", 0)
 
         iv_fish.setImageResource(image)
         tv_fish_name.text = name
 
+        val result: RealmResults<Model> = realm.where<Model>()
+            .equalTo("title", name)
+            .findAll()
 
+        tv_size_result.text = result[0]?.size
+        tv_weight_result.text =result[0]?.weight
+        tv_body_color_result.text = result[0]?.bodyColor
+        tv_spawning_season_result.text =result[0]?.spawningSeason
+        tv_habitat_result.text = result[0]?.habitat
+        tv_distribution_area_result.text =result[0]?.distributionArea
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
     }
 }
